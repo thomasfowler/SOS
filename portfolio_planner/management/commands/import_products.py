d3b764316853e9877bf1f7b238ac801f00fecab7
Thomas Fowler <thomas@dotmodus.com>
2023-12-19 21:57:21 +0200

    Created Product import command

--- portfolio_planner/management/commands/import_products.py
+++ portfolio_planner/management/commands/import_products.py
@@ -1,15 +1,15 @@
-"""Management command to import media groups from a CSV file."""
+"""Management command to import products from a CSV file."""
 from django.core.management.base import BaseCommand, CommandError
-from portfolio_planner.models import MediaGroup
+from portfolio_planner.models import Product
 import csv
 
 
 class Command(BaseCommand):
-    """Imports media groups from a CSV file."""
+    """Imports products from a CSV file."""
     help = 'Imports media groups from a CSV file'
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', type=str, help='Path to the CSV file containing media group data')
+        parser.add_argument('csv_file', type=str, help='Path to the CSV file containing product data')
 
     def handle(self, *args, **kwargs):
         csv_file_path = kwargs['csv_file']
@@ -19,11 +19,11 @@ class Command(BaseCommand):
             with open(csv_file_path, newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                    media_group = MediaGroup.objects.create(
+                    product = Product.objects.create(
                         name=row['Name'],
                         description=row['Description']
                     )
 
-                    self.stdout.write(self.style.SUCCESS(f'Successfully created media group {media_group.name}'))
+                    self.stdout.write(self.style.SUCCESS(f'Successfully created product {product.name}'))
         except FileNotFoundError:
             raise CommandError(f'File "{csv_file_path}" does not exist')
