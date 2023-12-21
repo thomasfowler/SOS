c2f4a68f44b6f4739f3c051540071d7114455ebb
Thomas Fowler <thomas@dotmodus.com>
2023-12-21 11:01:23 +0200

    Improve User import command to properly handle passwords and update functionality

--- portfolio_planner/management/commands/import_users.py
+++ portfolio_planner/management/commands/import_users.py
@@ -1,9 +1,9 @@
 """Management command to import users from a CSV file."""
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import Group
+from django.db import IntegrityError, transaction
 from portfolio_planner.models import User
 import csv
-
 
 class Command(BaseCommand):
     """Imports users from a CSV file."""
@@ -12,6 +12,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('csv_file', type=str, help='Path to the CSV file containing user data')
 
+    @transaction.atomic
     def handle(self, *args, **kwargs):
         csv_file_path = kwargs['csv_file']
 
@@ -20,18 +21,36 @@ class Command(BaseCommand):
             with open(csv_file_path, newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                    user = User.objects.create_user(
-                        email=row['Email Address'],
-                        password=row['Password'],
-                        first_name=row['First Name'],
-                        last_name=row['Last Name']
-                    )
+                    try:
+                        # Update or create the user based on email
+                        user, created = User.objects.update_or_create(
+                            email=row['Email Address'],
+                            defaults={
+                                'first_name': row['First Name'],
+                                'last_name': row['Last Name'],
+                                # Don't set password here; handle it after user creation/update
+                            }
+                        )
 
-                    # Assign user to group
-                    group_name = row['Role']
-                    group = Group.objects.get(name=group_name)
-                    user.groups.add(group)
+                        # Set password securely
+                        if created:
+                            user.set_password(row['Password'])
+                            user.save()
 
-                    self.stdout.write(self.style.SUCCESS(f'Successfully created user {user.email}'))
+                        # Assign user to group
+                        group_name = row['Role']
+                        try:
+                            group = Group.objects.get(name=group_name)
+                            user.groups.add(group)
+                        except Group.DoesNotExist:
+                            self.stdout.write(self.style.ERROR(f'Group {group_name} does not exist'))
+
+                        action = "created" if created else "updated"
+                        self.stdout.write(self.style.SUCCESS(f'Successfully {action} user {user.email}'))
+
+                    except IntegrityError as e:
+                        self.stdout.write(self.style.ERROR(f'Error importing user {row["Email Address"]}: {e}'))
+                        continue
+
         except FileNotFoundError:
             raise CommandError(f'File "{csv_file_path}" does not exist')
